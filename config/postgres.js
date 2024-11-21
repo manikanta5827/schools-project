0d120bb6d6fd93f7d2c6356807c014b50c733bde
Abbay <postbox5827@gmail.com>
2024-11-21 18:11:58 +0530

    env var

--- config/postgres.js
+++ config/postgres.js
@@ -1,13 +1,20 @@
 const { Pool } = require('pg');
 
+// Environment variables
 const connectionString = process.env.DATABASE_URL;
+const isProduction = process.env.NODE_ENV === 'production';
 
+// PostgreSQL pool configuration
 const pool = new Pool({
   connectionString,
+  ssl: isProduction ? { rejectUnauthorized: false } : false, 
 });
 
 pool.connect((err) => {
-  if (err) throw err;
+  if (err) {
+    console.error('Error connecting to PostgreSQL database', err.stack);
+    throw err;
+  }
   console.log('Connected to PostgreSQL database');
 });
 
